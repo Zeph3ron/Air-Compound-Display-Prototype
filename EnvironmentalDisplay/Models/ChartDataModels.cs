1af75c9217f4179e655efa30d05832b3bd951cc4
Kristinn <kk88yea@hotmail.com>
2016-04-25 14:09:21 +0200

    added some EntityFramework

---
+++ EnvironmentalDisplay/Models/ChartDataModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNet.Highcharts.Helpers;
+using DotNet.Highcharts.Options;
+using EnvironmentalDisplay.EF;
+
+namespace EnvironmentalDisplay.Models
+{
+    public static class ChartDataModels
+    {
+        public static object[] ReturnSomething(List<Setup> setups)
+        {
+            
+            return setups.Select(s => new { A = s.Location.LocationName, B = s.Compound.CompoundName}).ToArray();
+        }
+    }
+}
---
+++ EnvironmentalDisplay/Models/DateResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnvironmentalDisplay.Models
+{
+    public class DateResult
+    {
+        public DateTime DateTime { get; set; }
+        public string Result { get; set; }
+    }
+}
